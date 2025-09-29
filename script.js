7c00d5f9db0b8e6bae5baaf298f8c912ec26a31b
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2025-09-29 08:54:11 +0330

    Improve my final World-Clock project

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
 function updateClock() {
-    let mainLeft = document.querySelector("#main-left1 h3");
+
+    let mainLeft1 = document.querySelector("#main-left1 h3");
     let firstTime = moment().tz("Asia/Tehran").format("hh:mm:ss [<small>]A[</small>]");
-    mainLeft.innerHTML = firstTime;
+    if (mainLeft1) mainLeft1.innerHTML = firstTime;
 
     let mainLeft2=document.querySelector("#main-left2 h3")
     let secondTime=moment().tz("Africa/Cairo").format("hh:mm:ss [<small>]A[</small>]")
-    mainLeft2.innerHTML=secondTime
+    if (mainLeft2) mainLeft2.innerHTML=secondTime
 
     let mainLeft3=document.querySelector("#main-left3 h3")
     let thirdTime=moment().tz("Europe/Zurich").format("hh:mm:ss [<small>]A[</small>]")
-    mainLeft3.innerHTML=thirdTime
+    if (mainLeft3) mainLeft3.innerHTML=thirdTime
 
 };
 updateClock();
-setInterval(updateClock, 1000);
+window.clockInterval=setInterval(updateClock, 1000);
 
 function updateDate(){
     let mainRight1=document.querySelector("#main-right1 span")
     let firstDate=moment().tz("Asia/Tehran").format("dddd, MMMM Do YYYY")
-    mainRight1.innerHTML=firstDate
+    if (mainRight1)mainRight1.innerHTML=firstDate
    
     let mainRight2=document.querySelector("#main-right2 span")
     let secondDate=moment().tz("Africa/Cairo").format("dddd, MMMM Do YYYY")
-    mainRight2.innerHTML=secondDate
+     if (mainRight2)mainRight2.innerHTML=secondDate
     
     let mainRight3=document.querySelector("#main-right3 span")
     let thirdDate=moment().tz("Europe/Zurich").format("dddd, MMMM Do YYYY")
-    mainRight3.innerHTML=thirdDate
+    if (mainRight3)mainRight3.innerHTML=thirdDate
 
 };
 updateDate();
 
 
 function selectCity(event){
-
- clearInterval(updateClock);
     
     let selectTarget=event.target.value;
     let mainElementCall=document.querySelector("main")
-    // let targetCity=document.querySelector("#select option")
     let momentLibrary=moment().tz(selectTarget);
-
     let selectedOption = event.target.options[event.target.selectedIndex];
     let cityName = selectedOption.textContent;
-
-
+   
     mainElementCall.innerHTML=`
     <div class="forth-contry">
         <div class="main-right" id="main-right4">
@@ -57,19 +53,22 @@ function selectCity(event){
     </div>
 
 `
+if (window.clockInterval) {
+  clearInterval(window.clockInterval);
+}
     
-    window.cityInterval = setInterval(() => {
-        let updatedTime = moment().tz(selectTarget).format("hh:mm:ss [<small>]A[</small>]");
+if (window.cityInterval) {
+        clearInterval(window.cityInterval);
+    }
+    
+window.cityInterval = setInterval(() => {
+        let updatedTime = moment().tz(selectTarget).format("hh:mm:ss")+"<small>" + moment().tz(selectTarget).format("A") + "</small>";
         let mainLeft4 = document.querySelector("#main-left4 h3");
         if (mainLeft4) {
             mainLeft4.innerHTML = updatedTime;
-
-        let mainRight4=document.querySelector("#main-right4 span")
-        let forthDate=moment().tz(selectTarget).format("dddd, MMMM Do YYYY")
-        if(mainRight4){
-            mainRight4.innerHTML=forthDate
         }
-        }
-    }, 1000);}
+    }, 1000);
+};
+    
 let selectElementChanged=document.querySelector("#select")
-selectElementChanged.addEventListener("change",selectCity)
+selectElementChanged.addEventListener("change",selectCity);
